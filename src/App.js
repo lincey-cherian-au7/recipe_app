39e02236dfa3209b0102f9d1f4929b25615bade1
lincey-cherian-au7 <linceycherian@gmail.com>
2020-11-09 08:58:45 +0530

    Added main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React,{Component} from 'react';
 import './App.css';
 import Search from './components/Search'
+import Product from './components/Product'
+
 
 
 class App extends Component{
-  getRecipe =(e)=>{
-    const recipe = e.target.elements.recipe.value;
+  state ={
+    results:[]
+  }
+
+  getRecipe = async(e)=>{
     e.preventDefault()
-    console.log(recipe)
+    const recipe = e.target.elements.recipe.value;
+
+    const result = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=${recipe}&app_id=8fa613f2&app_key=aedc7f2e03cf64d4a13da7611b9d008d`);
+    const data = await result.json();
+    console.log(data.hits);
+    this.setState({results:data.hits})
+    
   }
   render(){
     return (
@@ -16,6 +27,8 @@ class App extends Component{
             <h1 className="App-title">Recipe App</h1>
           </header>
           <Search getRecipe={this.getRecipe}/>
+          <br/>
+          <Product results={this.state.results}/>
         </div>
     );
     
